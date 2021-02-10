08d54bbeea2b2ca8051cb926289b7cee926445d2
Daniel Collins <daniel.collins11@spsmail.cuny.edu>
2021-02-09 23:09:17 -0500

    My first commit

---
+++ assignment1_part1.py
@@ -0,0 +1,15 @@
+class ListDivideException(Exception):
+    pass
+def testListDivide():
+    print("calling listDivide")
+    def listDivide(numbers, divide=2):
+        amt = 0
+        for num in numbers:
+            if(num % divide == 0):
+                amt += 1
+        return amt
+print(listDivide([1,2,3,4,5]))
+print(listDivide([2,4,6,8,10]))
+print(listDivide([30, 54, 63,98,100],10))
+print(listDivide([]))
+print(listDivide([1,2,3,4,5],1))
